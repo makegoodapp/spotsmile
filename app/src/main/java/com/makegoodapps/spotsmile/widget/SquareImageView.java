816dd5feaaa0c2687362d11ac7ef6d8e93b55393
ChihWeiLee <rm4xu6@yahoo.com.tw>
2014-12-21 15:15:29 +0800

    Finish basic features

---
+++ app/src/main/java/com/makegoodapps/spotsmile/widget/SquareImageView.java
@@ -0,0 +1,29 @@
+package com.makegoodapps.spotsmile.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * A custom ImageView to implement square image
+ * Created by WillsPC on 2014/12/21.
+ */
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
+    }
+}
